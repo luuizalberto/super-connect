7dfc5fffb4c3d8c1a6104d112f9b237712dbbb41
Luiz Alberto <79115384+luuizalberto@users.noreply.github.com>
2023-08-10 16:18:38 -0300

    tela Contratos pronta

--- src/screens/Contratos/index.js
+++ src/screens/Contratos/index.js
@@ -1,9 +1,20 @@
-import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { useState } from "react";
 import { Feather } from "@expo/vector-icons";
+import CarregarFaturas from "../../components/CarregarFaturas";
+import OpenBoxContratos2 from "../../components/OpenBoxContratos2";
+import CarregarContratos from "../../components/CarregarContratos";
+import ConteudoTermos2 from "../../components/ConteudoTermos2";
 
-export default function Contratos({ navigation }) {
+export default function Faturas({ navigation }) {
   const openMenu = () => {
     navigation.openDrawer();
+  };
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleToggleVisibility = () => {
+    setIsVisible(!isVisible);
   };
 
   return (
@@ -25,6 +36,77 @@ export default function Contratos({ navigation }) {
           <Feather name="menu" size={30} color={"black"} />
         </TouchableOpacity>
       </View>
+      <View style={styles.bodyMain}>
+        <View style={[styles.boxMaior, { backgroundColor: "#A3009E" }]}>
+          <View style={styles.conteudoLeft}>
+            <Text style={styles.textMaior}>Contratos</Text>
+            <Text
+              style={[
+                styles.textMaior,
+                { marginTop: 5, fontSize: 14, fontWeight: 300, width: "90%" },
+              ]}
+            >
+              gerencie seus contratos.
+            </Text>
+          </View>
+          <View style={styles.conteudoRight}>
+            <Text style={[styles.textMaior, { fontSize: 22 }]}>1</Text>
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: 14,
+                fontWeight: 300,
+                textAlign: "right",
+              }}
+            >
+              contratos ativos listados
+            </Text>
+          </View>
+        </View>
+
+        <CarregarContratos />
+      </View>
+      {/* 
+      
+      
+      
+      */}
+      <View style={styles.bodyMain}>
+        <View style={[styles.boxMaior, { backgroundColor: "#61025e" }]}>
+          <View style={styles.conteudoLeft}>
+            <Text style={styles.textMaior}>Termos</Text>
+            <Text
+              style={[
+                styles.textMaior,
+                { marginTop: 5, fontSize: 14, fontWeight: 300, width: "90%" },
+              ]}
+            >
+              gerencie seus termos.
+            </Text>
+          </View>
+          <View style={styles.conteudoRight}>
+            <Text style={[styles.textMaior, { fontSize: 22 }]}>1</Text>
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: 14,
+                fontWeight: 300,
+                textAlign: "right",
+              }}
+            >
+              termos listados
+            </Text>
+          </View>
+        </View>
+        {/* 
+        ----------------------- CARREGAR
+        */}
+
+        <ConteudoTermos2
+          status={"assinado"}
+          descricao={"30_MB_R$_49,90__MIGRACAO"}
+        />
+      </View>
     </View>
   );
 }
@@ -33,4 +115,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  bodyMain: {
+    backgroundColor: "#fff",
+    marginTop: 25,
+    marginBottom: 10,
+    width: "98%",
+    padding: 15,
+    left: "1%",
+    minHeight: 50,
+    elevation: 20,
+    flexDirection: "column",
+  },
+  boxMaior: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#139e3a",
+    borderRadius: 4,
+    top: -30,
+    marginBottom: -25,
+    elevation: 10,
+  },
+
+  // Conteúdo dentro da BOX do TITULO
+  textMaior: {
+    color: "#fff",
+    fontSize: 22,
+    // width: 100,
+  },
+  conteudoRight: { width: "32%", alignItems: "flex-end" },
 });
